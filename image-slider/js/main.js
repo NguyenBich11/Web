761b5cb0100f4c944b01b7760c69b2959e538473
NguyenBich11 <ntngocbich11122004@gmail.com>
2023-08-20 16:23:32 +0700

    image-slide

---
+++ image-slider/js/main.js
@@ -0,0 +1,9 @@
+document.getElementById('next').onclick = function() {
+      let lists = document.querySelectorAll('.item');
+      document.getElementById('slider').appendChild(lists[0]);
+};
+
+document.getElementById('prev').onclick = function() {
+      let lists = document.querySelectorAll('.item');
+      document.getElementById('slider').prepend(lists[lists.length-1]);
+};
